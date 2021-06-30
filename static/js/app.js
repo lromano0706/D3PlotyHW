854430e41ccb2f23ac0ffa9e6ac8609feca6c4e9
lromano0706 <78659955+lromano0706@users.noreply.github.com>
2021-06-29 18:00:21 -0700

    tutor

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,35 @@
+// Made variable for json call for data
 var jsonFile = d3.json('data/samples.json')
+var demoOutput = d3.select("#sample-metadata");
+// made function to call values for data.names to append into drop down menu
+function dropDownlist(){
 var dropMenu = d3.select('#selDataset')
-  var subjectId = jsonFile.then(function(data){
-  data.names.map(function(idNo){
-    dropMenu.append('option').text(idNo)
-  })
-})
+jsonFile.then(data => {
+  data.names.forEach(id => dropMenu.append('option').text(id))
+  
+});
+};
 
+
+// called function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dropDownlist();
